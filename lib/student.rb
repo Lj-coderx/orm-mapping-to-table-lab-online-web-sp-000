7c620edeb505a67cd499d71af755dfdf5af74c78
Lj <tnedutsl457892@gmail.com>
2021-06-28 01:52:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,76 +1,46 @@
 class Student
-  attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+  # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
-  def self.all
+  def self.create_table
     sql = <<-SQL
-    SELECT *
-    FROM students
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql)
   end
 
-  def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE name = ?
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-  def save
-	@@ -40,4 +56,41 @@ def self.drop_table
+  def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
 
-  def self.students_below_12th_grade
+  def save
     sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE grade < 12
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE grade = 10
-    ORDER BY id
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.first
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
-
-  def self.count_all_students_in_grade_9
-    sql = <<-SQL
-    SELECT COUNT(grade)
-    FROM students
-    WHERE grade = 9
-    SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
 
 end
